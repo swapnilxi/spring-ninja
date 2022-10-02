a182aafb12cf9c3ccf10cfe0afd16933e7cb5a79
swapnil gupta <swapnilg671@gmail.com>
2022-10-03 03:43:01 +0530

    form in spring

--- grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
+++ grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -1,30 +1,58 @@
 package com.ltp.gradesubmission;
 
-import java.util.ArrayList;
+
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class GradeController {
-    List<Grade> studentGrades= new ArrayList<>();
+    List<Grade> studentGrades=  Arrays.asList(
+    new Grade("swapnil", "computerVision", "a"),
+        
+    new Grade("swapnil", "computerVision", "a"),
+        
+    new Grade("darshit", "java", "a2")
+    );
     
     @GetMapping("/grades")
     public String getGrades(Model model)
     {
-        studentGrades.add(new Grade("swapnil", "computerVision", "a"));
-        
-        studentGrades.add(new Grade("swapnil", "computerVision", "a"));
-        
-        studentGrades.add(new Grade("darshit", "java", "a2"));
 
-        
        // Grade grade= new Grade("swapnil", "history", "a1");
        model.addAttribute("grade", studentGrades );
         return "grades";
     }
+    @GetMapping("/form")
+        public String gradeForm(Model model){
+          model.addAttribute("grade", new Grade("swapnil", "chemistry", "A1"));
+            return "form";
+        }
+    
+    }
+
+
+
+
+
+// @GetMapping("/grades")
+// public String getGrades(Model model)
+// {
+//     studentGrades.add(new Grade("swapnil", "computerVision", "a"));
+    
+//     studentGrades.add(new Grade("swapnil", "computerVision", "a"));
+    
+//     studentGrades.add(new Grade("darshit", "java", "a2"));
 
     
-}
+//    // Grade grade= new Grade("swapnil", "history", "a1");
+//    model.addAttribute("grade", studentGrades );
+//     return "grades";
+// }
+
+
+// }
