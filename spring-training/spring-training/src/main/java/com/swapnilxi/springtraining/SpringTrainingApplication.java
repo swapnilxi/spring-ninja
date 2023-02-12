457bb17bd9ac21cfd44271d234696d8f6d9776a2
swapnil <swapnilg671@gmail.com>
2023-02-12 19:06:37 +0530

    spring-training

---
+++ spring-training/spring-training/src/main/java/com/swapnilxi/springtraining/SpringTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.swapnilxi.springtraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTrainingApplication.class, args);
+	}
+
+}
---
+++ spring-training/spring-training/src/test/java/com/swapnilxi/springtraining/SpringTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swapnilxi.springtraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
