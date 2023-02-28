e73270ab769d20672078cb1be09ff8c4f3156472
swapnil <swapnilg671@gmail.com>
2023-02-28 15:36:32 +0530

    course api

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # spring-ninja
 This Repository will teach you about spring and Spring boot projects 
 # Project1 - Grade-Submission
+# Project 2-  courseApi
---
+++ courseapi/src/main/java/com/swapnilxi/courseapi/CourseapiApplication.java
@@ -0,0 +1,13 @@
+package com.swapnilxi.courseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseapiApplication.class, args);
+	}
+
+}
